8abbab812df95ebc2ef10c981c5d5f2c4526b4a8
msagr <mehulsagrawal33@gmail.com>
2025-08-16 18:55:42 +0530

    feat: automatic creation of user profile on registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db.models import OneToOneField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -97,4 +99,17 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.email 
-    
+
+
+@receiver(post_save, sender = User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user = instance)
+    else:
+        try:
+            profile = UserProfile.objects.get(user = instance)
+            profile.save()
+        except:
+            UserProfile.objects.create(user = instance)
+
+#post_save.connect(post_save_create_profile_receiver, sender = User)
