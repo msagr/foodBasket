c7547647693d69732705a4e80eedbfd9a78776df
msagr <mehulsagrawal33@gmail.com>
2025-08-17 20:48:37 +0530

    feat: implement Django model form for user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserForm
 
 # Create your views here.
 def registerUser(request):
-    return render(request, 'accounts/registerUser.html')
+    form = UserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/registerUser.html', context)
