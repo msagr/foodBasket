d4e65c261d65058c0dea63fca4238c9ae7e4290f
msagr <mehulsagrawal33@gmail.com>
2025-08-18 11:36:00 +0530

    security: implemented password hashing

--- accounts/views.py
+++ accounts/views.py
@@ -8,9 +8,23 @@ def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.role = User.CUSTOMER
+            # User creation using form 
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+
+            # User creation using create_user method 
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER 
             user.save()
+
             return redirect('registeruser')
     else:
         form = UserForm()
