113469fcdf8b519d0bcf003dc6c23d00da1bf4f9
msagr <mehulsagrawal33@gmail.com>
2025-08-18 11:18:37 +0530

    feat: save role field as customer before form submission

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserForm
+from .models import User
 
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.save()
             return redirect('registeruser')
     else:
         form = UserForm()
