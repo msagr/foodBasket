d6ef6d8c033b194ce0ace08b1d8979304a83adf5
msagr <mehulsagrawal33@gmail.com>
2025-08-18 12:56:11 +0530

    fix: remove non-existent field from User fields of User form

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ class UserForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'confirm_password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
 
     def clean(self):
         cleaned_data = super(UserForm, self).clean()
